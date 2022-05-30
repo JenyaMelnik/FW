36366bc500b2b7b1b130090197b9e7bd66dcfa09
JenyaMelnik <Jenyamelnik1986@i.ua>
2022-05-30 21:45:58 +0300

    empty module api was added

---
+++ modules/api/sitemap/sitemap.php
@@ -0,0 +1,98 @@
+<?php
+
+return [
+    'auth' => [
+        'page2' => [],
+        'login' => [],
+        'password' => [],
+    ],
+
+    'socials' => [
+        'page2' => [],
+        'param1' => [],
+    ],
+
+    '/OPTIONS' => [
+        'controller' => true,
+    ]
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+return [
+    'auth' => [
+        'page2' => [
+            'default' => 'main',
+        ],
+        'param1' => [],
+    ],
+
+    'socials' => [
+        'page2' => [
+            'default' => 'main'
+        ],
+        'delete' => [
+            'param1' => [],
+        ],
+
+//        'param2' => [],
+    ],
+
+    '/OPTIONS' => [
+        'controller' => true,
+    ]
+];
+
+
+
+
+
+'news' => array(
+    'view' => array(
+        'GET' => array(
+            'category' => array(
+                'rules' => '[a-zа-яё0-9_-]+',
+                'default' => 'all',
+                'type' => 'string',
+            ),
+            'page' => array(
+                'rules' => '[0-9]+',
+                'default' => 1,
+                'type' => 'int',
+            ),
+        ),
+    ),
+    'read' => array(
+        'GET' => array(
+            'id' => array(
+                'req' => 1,
+                'rules' => '[0-9]+',
+            ),
+        ),
+    ),
+),
+*/
