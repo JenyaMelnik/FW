156bdba2f1a7fb5d8b00597c11127b1c8d4dd9b8
JenyaMelnik <Jenyamelnik1986@i.ua>
2022-06-11 01:09:39 +0300

    feat: homework 15 done

--- library/Api/IApi.php
+++ library/Api/IApi.php
@@ -4,33 +4,5 @@ namespace Api;
 
 interface IApi
 {
-    /**
-     * @param string $correctMethod
-     */
-    public function verifyRequestMethod(string $correctMethod): void;
-
-    /**
-     *
-     */
-    public function verifyLoginAndPassword(): void;
-
-    /**
-     *
-     */
-    public function createSecretToken(): void;
-
-    /**
-     *
-     */
-    public function verifyApiToken(): void;
-
-    /**
-     *
-     */
-    public function printConnectedSocials(): void;
-
-    /**
-     *
-     */
-    public function deleteAuthViaSocial(): void;
+    public function printResponse(array $response): void;
 }
